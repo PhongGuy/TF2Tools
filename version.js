82326eaa8902cabc4808d1914dfc3cb8bbd556ec
Daniel Olesen <dto@outlook.dk>
2021-12-23 16:59:16 +0100

    added easy version change

---
+++ version.js
@@ -0,0 +1,38 @@
+const { error, log } = require('console');
+const fs = require('fs');
+
+const p1Path = './package.json';
+const p2Path = './app/package.json';
+const envPath = './src/environments'
+
+const newVersion = process.argv[2];
+
+if (newVersion === undefined) {
+    error('new version has to be specified')
+} else {
+
+    // update package.json
+    const p1 = fs.readFileSync(p1Path, { encoding: 'utf8', flag: 'r' });
+    const p2 = fs.readFileSync(p2Path, { encoding: 'utf8', flag: 'r' });
+
+    let v1 = p1.split('\n').filter(a => a.includes('"version":'))[0];
+    let v2 = p2.split('\n').filter(a => a.includes('"version":'))[0];
+
+    let v = v1.replace('version', '').replace(/"/g, '').replace(/:/g, '').replace(/,/g, '').replace(/ /g, '');
+
+    let newV1 = v1.replace(v, newVersion);
+    let newV2 = v2.replace(v, newVersion);
+
+    fs.writeFileSync(p1Path, p1.replace(v1, newV1));
+    fs.writeFileSync(p2Path, p2.replace(v2, newV2));
+
+    // update APP_CONFIG
+    const envs = fs.readdirSync(envPath);
+
+    envs.forEach(env => {
+        const p = `${envPath}/${env}`;
+        const e = fs.readFileSync(p, { encoding: 'utf8', flag: 'r' });
+        fs.writeFileSync(p, e.replace(v, newVersion));
+        log(`Updated ${env}`);
+    })
+}
