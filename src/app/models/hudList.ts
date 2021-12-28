2126270ad04a36074ce3ca8bf9f62280fb0ecd28
Daniel Olesen <dto@outlook.dk>
2021-12-28 15:14:18 +0100

    added doc to HudList

--- src/app/models/hudList.ts
+++ src/app/models/hudList.ts
@@ -1,12 +1,42 @@
+/**
+ * Hud list
+ */
 export class HudList {
     name: string;
+    /**
+     * Creatorof hud list
+     */
     creator: string;
+    /**
+     * Maintainerof hud list
+     */
     maintainer: string;
+    /**
+     * Screenshotsof hud list
+     */
     screenshots: string;
+    /**
+     * Pageof hud list
+     */
     page: string;
+    /**
+     * Repositoryof hud list
+     */
     repository: string;
+    /**
+     * Groupof hud list
+     */
     group: string;
+    /**
+     * Discussionof hud list
+     */
     discussion: string;
+    /**
+     * Downloadof hud list
+     */
     download: string;
+    /**
+     * Discordof hud list
+     */
     discord: string;
 }
