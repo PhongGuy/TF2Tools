2edb1adf929056b5a53c227e66fc6336abba6be2
Daniel Olesen <dto@outlook.dk>
2021-12-28 14:54:31 +0100

    added doc to UploadChangeNameComponent

--- src/app/dashboard/hitsound/upload-change-name/upload-change-name.component.ts
+++ src/app/dashboard/hitsound/upload-change-name/upload-change-name.component.ts
@@ -1,19 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UploadChangeName } from '../../../models/uploadChangeName';
 import { ValidatorService } from '../../../services/validators.service';
 
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  /** Check if error state */
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
+/**
+ * Upload change name
+ */
 @Component({
   selector: 'app-upload-change-name',
   templateUrl: './upload-change-name.component.html',
@@ -21,26 +15,51 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class UploadChangeNameComponent implements OnInit {
 
+  /**
+   * Input answer
+   */
   input: FormControl;
-  matcher: MyErrorStateMatcher;
+  /**
+   * Matcher of upload change name component
+   */
+  matcher: ShowOnDirtyErrorStateMatcher;
 
+  /**
+   * Creates an instance of upload change name component.
+   *
+   * @param dialogRef
+   * @param data UploadChangeName
+   * @param validator
+   */
   constructor(
-    public dialogRef: MatDialogRef<UploadChangeNameComponent>,
+    private dialogRef: MatDialogRef<UploadChangeNameComponent>,
     @Inject(MAT_DIALOG_DATA) public data: UploadChangeName,
     private validator: ValidatorService
   ) { }
 
+  /**
+   * on init
+   */
   ngOnInit(): void {
-    this.matcher = new MyErrorStateMatcher();
+    this.matcher = new ShowOnDirtyErrorStateMatcher();
     this.input = new FormControl('', [Validators.required, this.validator.checkName(this.data.cant), this.validator.special()]);
   }
 
+  /**
+   * Submits upload change name component
+   */
   submit() {
     if (!this.input.invalid) {
       this.dialogRef.close(this.input.value);
     }
   }
 
+  /**
+   * Plays file
+   *
+   * @param event
+   * @param file
+   */
   playFile(event: Event, file: File) {
     event.preventDefault();
     const audio = new Audio(file.path);
