93efb4022dfe841f85226a2cffc51106fe753bc9
Daniel Olesen <dto@outlook.dk>
2021-12-28 15:11:57 +0100

    added doc to CrossahirSelected

--- src/app/models/crosshairSelected.ts
+++ src/app/models/crosshairSelected.ts
@@ -1,8 +1,28 @@
-
+/**
+ * Crossahir selected
+ */
 export class CrossahirSelected {
+    /**
+     * Classes  of crossahir selected
+     */
     classes: string[] = [];
+    /**
+     * Weapon classes of crossahir selected
+     */
+    /**
+     * Weapon classes of crossahir selected
+     */
     weaponClasses: string[] = [];
+    /**
+     * Names  of crossahir selected
+     */
     names: string[] = [];
+    /**
+     * Slots  of crossahir selected
+     */
     slots: string[] = [];
+    /**
+     * Weapons affected of crossahir selected
+     */
     weaponsAffected: string[] = [];
 }
