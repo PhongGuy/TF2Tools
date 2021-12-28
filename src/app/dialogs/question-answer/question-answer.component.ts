69c41c6ac5502c4d2427857436645e15b30e30ec
Daniel Olesen <dto@outlook.dk>
2021-12-28 14:10:05 +0100

    added doc to QuestionAnswerComponent

--- src/app/dialogs/question-answer/question-answer.component.ts
+++ src/app/dialogs/question-answer/question-answer.component.ts
@@ -7,13 +7,22 @@ import { ValidatorService } from '../../services/validators.service';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  /** Check if error state */
+  /**
+   * Determines whether error state is
+   *
+   * @param control
+   * @param form
+   * @returns true if error state
+   */
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
 
+/**
+ * Component QuestionAnswerComponent
+ */
 @Component({
   selector: 'app-question-answer',
   templateUrl: './question-answer.component.html',
@@ -21,21 +30,39 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class QuestionAnswerComponent implements OnInit {
 
+  /**
+   * Input answer
+   */
   input: FormControl;
+  /**
+   * Matcher of question answer component
+   */
   matcher: MyErrorStateMatcher;
 
+  /**
+   * Creates an instance of question answer component.
+   *
+   * @param dialogRef
+   * @param data `QuestionAnswer`
+   * @param validator
+   */
   constructor(
-    public dialogRef: MatDialogRef<QuestionAnswerComponent>,
+    private dialogRef: MatDialogRef<QuestionAnswerComponent>,
     @Inject(MAT_DIALOG_DATA) public data: QuestionAnswer,
     private validator: ValidatorService
   ) { }
 
+  /**
+   * on init
+   */
   ngOnInit(): void {
     this.matcher = new MyErrorStateMatcher();
     this.input = new FormControl('', [Validators.required, this.validator.checkName(this.data.cant), this.validator.special()]);
   }
 
-  /** Submit input */
+  /**
+   * Submits question answer component
+   */
   submit() {
     if (!this.input.invalid) {
       this.dialogRef.close(this.input.value);
