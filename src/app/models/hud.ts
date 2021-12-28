7a552fa4a990f7ea32c0d501c488227c868c4090
Daniel Olesen <dto@outlook.dk>
2021-12-28 15:13:22 +0100

    added doc to Hud

--- src/app/models/hud.ts
+++ src/app/models/hud.ts
@@ -1,6 +1,21 @@
+/**
+ * Hud
+ */
 export class Hud {
+    /**
+     * Name  of hud
+     */
     name: string;
+    /**
+     * Folder name of hud
+     */
     folderName: string;
+    /**
+     * Version  of hud
+     */
     version: string;
+    /**
+     * Path  of hud
+     */
     path: string;
 }
