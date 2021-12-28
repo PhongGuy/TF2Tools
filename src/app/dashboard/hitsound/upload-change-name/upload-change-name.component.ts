2f6284f617089fe330b9f153f0963081d936a79e
Daniel Olesen <dto@outlook.dk>
2021-12-28 12:45:33 +0100

    added doc to MyErrorStateMatcher

--- src/app/dashboard/hitsound/upload-change-name/upload-change-name.component.ts
+++ src/app/dashboard/hitsound/upload-change-name/upload-change-name.component.ts
@@ -7,6 +7,7 @@ import { ValidatorService } from '../../../services/validators.service';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
+  /** Check if error state */
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
